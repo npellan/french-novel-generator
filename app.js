9468ef915cca27ebb39e7acfb558c3d2859eeada
npellan <npellan@gmail.com>
2020-12-27 10:28:01 +0100

    console.log working

--- app.js
+++ app.js
@@ -23,4 +23,40 @@ const app = {
         'tout son village',
         'son amant'
     ],
+    getRandomNumber: (min, max) => {
+        const randomBase = Math.random(); // Chiffre flottant entre 0 et 1
+        const randomNumber = randomBase * (max - min) + min; // Chiffre flottant entre min et max
+        const roundedRandomNumber = Math.round(randomNumber); // Chiffre entier entre min et max
+        return roundedRandomNumber;
+        // return Math.round(Math.random() * (max - min) + min);
+    },
+    getLieu: () => {
+        const nb = app.getRandomNumber(0, 3);
+        const oneLieu = app.lieu[nb];
+        return oneLieu;        
+    },
+    getPersonnage: () => {
+        const nb = app.getRandomNumber(0, 3);
+        const onePersonnage = app.personnage[nb];
+        return onePersonnage;        
+    },
+    getVerbe: () => {
+        const nb = app.getRandomNumber(0, 3);
+        const oneVerbe = app.verbe[nb];
+        return oneVerbe;        
+    },
+    getInteraction: () => {
+        const nb = app.getRandomNumber(0, 3);
+        const oneInteraction = app.interaction[nb];
+        return oneInteraction;        
+    },
+    getNovel: () => {
+        const lieu = app.getLieu();
+        const personnage = app.getPersonnage();
+        const verbe = app.getVerbe();
+        const interaction = app.getInteraction();
+        console.log(`${lieu}, ${personnage}, ${verbe[0]} ${interaction}, ${verbe[1]}`);
+    }
 }
+
+app.getNovel();
