0e5b8ea3fb54fbc7e519c49ee46fc5d4c378a237
npellan <npellan@gmail.com>
2020-12-27 10:15:15 +0100

    first data

---
+++ app.js
@@ -0,0 +1,26 @@
+const app = {
+    lieu: [
+        'Dans une vallée reculée des Alpes',
+        'A la pointe de la Bretagne',
+        'A la lisière d\'une forêt des Cévennes',
+        'Au milieu des plaines du Berry'
+    ],
+    personnage: [
+        'Mathieu, un ingénieur à la retraite',
+        'Paul, un ancien cadre reconverti à l\'agriculture',
+        'Louise, une architecte française renommée',
+        'Jeanne, une ancienne sportive'
+    ],
+    verbe: [
+        ['se dispute avec', 'à propos d\'une grange abandonnée'],
+        ['part parcourir la France avec', 'pour retrouver une amie d\'enfance'],
+        ['épie', 'toutes les nuits'],
+        ['tente de convaincre', 'de se présenter aux élections municipales']
+    ],
+    interaction: [
+        'sa femme', 
+        'son voisin',
+        'tout son village',
+        'son amant'
+    ],
+}
